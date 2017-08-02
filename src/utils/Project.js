87f591ddea1f3623458cfa0487e6c74c19ca218f
dragonman225 <zxcvb22217@gmail.com>
2017-08-02 23:47:35 +0800

    :art: Project class with newOne and vote

---
+++ src/utils/Project.js
@@ -0,0 +1,68 @@
+/*
+class Project {
+constructor(Object project) {};
+newOne();
+vote(String userId, Array dates);
+update(String userId, Number deadline);
+remove(String userId);
+getStats(String userId);
+}
+*/
+import axios from 'axios';
+
+class Project {
+  constructor(project) {
+    this.id = project.id;
+    this.team = project.team;
+    this.title = project.title;
+    this.minDuration = project.minDuration;
+    this.description = project.description;
+    this.location = project.location;
+    this.finaldate = project.finaldate;
+    this.deadline = project.deadline;
+    this.ended = project.ended;
+    this.superuser = project.superuser;
+    this.normaluser = project.normaluser;
+    this.optionaluser = project.optionaluser;
+    this.closedUser = project.closedUser;
+    this.votes = project.votes;
+  }
+  newOne() {
+    axios.post('/api/project/new', {
+      id: this.id,
+      team: this.team,
+      title: this.title,
+      minDuration: this.minDuration,
+      description: this.description,
+      location: this.location,
+      finaldate: this.finaldate,
+      deadline: this.deadline,
+      ended: this.ended,
+      superuser: this.superuser,
+      normaluser: this.normaluser,
+      optionaluser: this.optionaluser,
+      closedUser: this.closedUser,
+      votes: this.votes,
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+  vote(userId, dates) {
+    axios.post(`/api/project/vote/${userId}`, {
+      projectId: this.id,
+      dates,
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+}
+
+export default Project;
