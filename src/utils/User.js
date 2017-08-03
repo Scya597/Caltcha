fcbe542c11c64c9550c32a42e07e54233fbf740e
Maximus Wu <stib108@yahoo.com.tw>
2017-08-03 10:59:41 +0800

    :rocket: finish util.js

--- src/utils/User.js
+++ src/utils/User.js
@@ -3,3 +3,13 @@ class User {
   constructor(Object user) {};
 }
 */
+
+export default class User {
+  constructor(user) {
+    this.id = user.id;
+    this.username = user.username;
+    this.realname = user.realname;
+    this.email = user.email;
+    this.team = user.team;
+  }
+}
