94ce79abd3f32ec0a4243f7a4c4fe2e83bcf15de
Maximus Wu <stib108@yahoo.com.tw>
2017-08-04 22:51:35 +0800

    :sparkles: finish vote function in vote.js

--- src/components/vote.js
+++ src/components/vote.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
-import '../scss/title.scss';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-class Vote extends Component {
+import '../scss/title.scss';
+
+export default class Vote extends Component {
   constructor(props) {
     super(props);
+
     this.state = {};
+    this.vote = this.vote.bind(this);
+  }
+
+  vote() {
+    const tmpDates = [
+      {
+        date: 20170728,
+        timeblocks: [1, 2, 3],
+      },
+      {
+        date: 20170811,
+        timeblocks: [6, 7, 8],
+      },
+    ];
+    axios.post('/api/project/vote/', {
+      projectId: this.props.match.params.id,
+      dates: tmpDates,
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   render() {
     return (
       <div>
+        <img src="http://www.motherjones.com/wp-content/uploads/silicon-valley.jpg" />
         <h1>vote</h1>
+        <button onClick={this.vote}>VOTE</button>
       </div>
-
     );
   }
 }
-export default Vote;
