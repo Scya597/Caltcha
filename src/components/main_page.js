7058bd773ef3ea1768c3ddc6c714c854ed6a2c32
cysyystiger <cysyystiger@gmail.com>
2017-08-05 23:14:18 +0800

    :art: set state in mainPage

--- src/components/main_page.js
+++ src/components/main_page.js
@@ -13,21 +13,38 @@ class MainPage extends Component {
       user: {},
       teams: [],
       projects: [],
+      superselectedpjs: [],
+      otherselectedpjs: [],
       selectedTeam: {},
     };
     this.fetchuser = this.fetchuser.bind(this);
     this.fetchprojects = this.fetchprojects.bind(this);
     this.setSelectedTeam = this.setSelectedTeam.bind(this);
-    this.super = this.super.bind(this);
-    this.voted = this.voted.bind(this);
-    this.novoted = this.novoted.bind(this);
+    // this.super = this.super.bind(this);
+    // this.voted = this.voted.bind(this);
+    // this.novoted = this.novoted.bind(this);
   }
   componentDidMount() {
     this.fetchuser();
     this.fetchprojects();
   }
   setSelectedTeam(teamObj) {
-    this.setState({ selectedTeam: teamObj });
+    const selectedpjs = [];
+    for (let i = 0; i < this.state.projects.length; i += 1) {
+      if (this.state.projects[i].team === teamObj.id) {
+        selectedpjs.push(this.state.projects[i]);
+      }
+    }
+    const superselectedpjs = [];
+    const otherselectedpjs = [];
+    for (let i = 0; i < selectedpjs.length; i += 1) {
+      if (this.state.user.id === selectedpjs[i]) {
+        superselectedpjs.push(selectedpjs[i]);
+      } else {
+        otherselectedpjs.push(selectedpjs[i]);
+      }
+    }
+    this.setState({ selectedTeam: teamObj, superselectedpjs, otherselectedpjs });
   }
   fetchuser = () => {
     axios.get('/api/profile')
@@ -56,93 +73,93 @@ class MainPage extends Component {
         console.log(err);
       });
   }
-  renderpjs = (arr) => {
-    const superarr = this.super(arr);
-    const votedarr = this.voted(arr);
-    const novotedarr = this.novoted(arr);
-    return (
-      <div>
-        {this.rendersuper(superarr)}
-        {this.rendervoted(votedarr)}
-        {this.rendernovoted(novotedarr)}
-      </div>
-    );
-  }
-  rendersuper = (superarr) => {
-    if (superarr.length !== 0) {
-      return (
-        <div>
-          <h4>Your project</h4>
-          <ul>{superarr.map(this.rendertitle)}</ul>
-        </div>
-      );
-    }
-  }
-  rendervoted = (votedarr) => {
-    if (votedarr.length !== 0) {
-      return (
-        <div>
-          <h4>You have voted</h4>
-          <ul>{votedarr.map(this.rendertitle)}</ul>
-        </div>
-      );
-    }
-  }
-  rendernovoted = (novotedarr) => {
-    if (novotedarr.length !== 0) {
-      return (
-        <div>
-          <h4>Your have not voted</h4>
-          <ul>{novotedarr.map(this.rendertitle)}</ul>
-        </div>
-      );
-    }
-  }
-  rendertitle = (obj) => {
-    return (
-      <li key={obj.id}>
-        <Link to={`/vote/${obj.id}/${this.state.user.id}/${obj.superuser}`}>
-          {obj.title}
-        </Link>
-      </li>
-    );
-  }
-  super = (arr) => {
-    const superarr = [];
-    for (let i = 0; i < arr.length; i += 1) {
-      if (this.state.user.id === arr[i].superuser) {
-        superarr.push(arr[i]);
-      }
-    }
-    return superarr;
-  }
-  voted = (arr) => {
-    const votedarr = [];
-    for (let i = 0; i < arr.length; i += 1) {
-      for (let j = 0; j < arr[i].votes.length; j += 1) {
-        if (this.state.user.id === arr[i].votes[j].userid && this.state.user.id !== arr[i].superuser) {
-          votedarr.push(arr[i]);
-        }
-      }
-    }
-    return votedarr;
-  }
-  novoted = (arr) => {
-    const novotedarr = [];
-    let b;
-    for (let i = 0; i < arr.length; i += 1) {
-      b = 1;
-      for (let j = 0; j < arr[i].votes.length; j += 1) {
-        if (this.state.user.id === arr[i].votes[j].userid) {
-          b = 0;
-        }
-      }
-      if (b) {
-        novotedarr.push(arr[i]);
-      }
-    }
-    return novotedarr;
-  }
+  // rendersuperpjs = (arr) => {
+  //   const superarr = this.super(arr);
+  //   // const votedarr = this.voted(arr);
+  //   // const novotedarr = this.novoted(arr);
+  //   return (
+  //     <div>
+  //       {this.rendersuper(superarr)}
+  //       {/* {this.rendervoted(votedarr)}
+  //       {this.rendernovoted(novotedarr)} */}
+  //     </div>
+  //   );
+  // }
+  // rendersuper = (superarr) => {
+  //   if (superarr.length !== 0) {
+  //     return (
+  //       <div>
+  //         <h4>Your project</h4>
+  //         <ul>{superarr.map(this.rendertitle)}</ul>
+  //       </div>
+  //     );
+  //   }
+  // }
+  // rendervoted = (votedarr) => {
+  //   if (votedarr.length !== 0) {
+  //     return (
+  //       <div>
+  //         <h4>You have voted</h4>
+  //         <ul>{votedarr.map(this.rendertitle)}</ul>
+  //       </div>
+  //     );
+  //   }
+  // }
+  // rendernovoted = (novotedarr) => {
+  //   if (novotedarr.length !== 0) {
+  //     return (
+  //       <div>
+  //         <h4>Your have not voted</h4>
+  //         <ul>{novotedarr.map(this.rendertitle)}</ul>
+  //       </div>
+  //     );
+  //   }
+  // }
+  // rendertitle = (obj) => {
+  //   return (
+  //     <li key={obj.id}>
+  //       <Link to={`/vote/${obj.id}/${this.state.user.id}/${obj.superuser}`}>
+  //         {obj.title}
+  //       </Link>
+  //     </li>
+  //   );
+  // }
+  // super = (arr) => {
+  //   const superarr = [];
+  //   for (let i = 0; i < arr.length; i += 1) {
+  //     if (this.state.user.id === arr[i].superuser) {
+  //       superarr.push(arr[i]);
+  //     }
+  //   }
+  //   return superarr;
+  // }
+  // voted = (arr) => {
+  //   const votedarr = [];
+  //   for (let i = 0; i < arr.length; i += 1) {
+  //     for (let j = 0; j < arr[i].votes.length; j += 1) {
+  //       if (this.state.user.id === arr[i].votes[j].userid && this.state.user.id !== arr[i].superuser) {
+  //         votedarr.push(arr[i]);
+  //       }
+  //     }
+  //   }
+  //   return votedarr;
+  // }
+  // novoted = (arr) => {
+  //   const novotedarr = [];
+  //   let b;
+  //   for (let i = 0; i < arr.length; i += 1) {
+  //     b = 1;
+  //     for (let j = 0; j < arr[i].votes.length; j += 1) {
+  //       if (this.state.user.id === arr[i].votes[j].userid) {
+  //         b = 0;
+  //       }
+  //     }
+  //     if (b) {
+  //       novotedarr.push(arr[i]);
+  //     }
+  //   }
+  //   return novotedarr;
+  // }
 
   render() {
     return (
@@ -156,10 +173,12 @@ class MainPage extends Component {
         <div className="row">
           <div className="col-md-4">
             <Link to="/new"><button className="btn btn-primary">Start a Project</button></Link>
-            {this.renderpjs(this.state.projects.filter(proj => proj.team === this.state.selectedTeam.id).filter(proj => proj.superuser === this.state.user.id))}
+            {/* {this.rendersuperpjs(this.state.selectedprojects)} */}
+            {/* {this.renderpjs(this.state.projects.filter(proj => proj.team === this.state.selectedTeam.id).filter(proj => proj.superuser === this.state.user.id))} */}
           </div>
           <div className="col-md-8">
-            {this.renderpjs(this.state.projects.filter(proj => proj.team === this.state.selectedTeam.id).filter(proj => proj.superuser !== this.state.user.id))}
+            {/* {this.renderpjs(this.state.selectedprojects)} */}
+            {/* {this.renderpjs(this.state.projects.filter(proj => proj.team === this.state.selectedTeam.id).filter(proj => proj.superuser !== this.state.user.id))} */}
           </div>
         </div>
       </div>
