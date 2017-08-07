16d25be8b9df9df789932972e2c560d396a6875d
cysyystiger <cysyystiger@gmail.com>
2017-08-07 17:12:54 +0800

    :art: build vote page

--- src/components/vote.js
+++ src/components/vote.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
-import Super from './super';
+import deadline from '../utils/functions/deadline';
 import EventData from './eventData';
 import '../scss/title.scss';
 
 export default class Vote extends Component {
   constructor(props) {
     super(props);
+    this.state = { project: {}, hourstoline: 0 };
 
-    const { userid, pjid, superid } = this.props.match.params;
-    this.state = {
-      userId: userid,
-      projectId: pjid,
-      superUserId: superid,
-    };
+    this.fetchpj = this.fetchpj.bind(this);
     this.vote = this.vote.bind(this);
-    this.checkSuper = this.checkSuper.bind(this);
+    this.updatepj = this.updatepj.bind(this);
+    this.removepj = this.removepj.bind(this);
+    //this.checkSuper = this.checkSuper.bind(this);
   }
-
+  componentDidMount() {
+    this.fetchpj();
+  }
+  fetchpj() {
+    const { pjid } = this.props.match.params;
+    axios.get(`/api/project/${pjid}`)
+      .then((res) => {
+        this.setState({ project: res.data, hourstoline: deadline(res.data.deadline) });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   vote() {
+    const { pjid } = this.props.match.params;
     const tmpDates = [
       {
         date: 20170728,
@@ -32,7 +42,7 @@ export default class Vote extends Component {
       },
     ];
     axios.post('/api/project/vote/', {
-      projectId: this.state.projectId,
+      projectId: pjid,
       dates: tmpDates,
     })
     .then((res) => {
@@ -43,43 +53,70 @@ export default class Vote extends Component {
     });
     this.props.history.push('/');
   }
+  updatepj() {
+    const { pjid } = this.props.match.params;
+    axios.post('/api/project/update', {
+      projectId: pjid,
+      ended: false,
+      deadline: 20170925,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    this.fetchpj();
+  }
+  removepj() {
+    const { pjid } = this.props.match.params;
+    axios.post('/api/project/remove', {
+      projectId: pjid,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    this.props.history.push('/');
+  }
 
-  checkSuper(user) {
-    if (user.userId !== user.superUserId) {
+  // checkSuper(user) {
+  //   if (user.userId !== user.superId) {
+  //     return (
+  //       <div>
+  //         <div className="list-border status-bar">
+  //           {'pick your time' || 'you have picked your time'}
+  //           <button className="btn btn-warning">edit users</button>
+  //         </div>
+  //         <button className="btn btn-default" onClick={this.vote}>VOTE</button>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <Super projectId={user.projectId} history={this.props.history} match={this.props.match} />
+  //     );
+  //   }
+  // }
+
+  render() {
+    const { userid } = this.props.match.params;
+    const { superid } = this.props.match.params;
+    if (userid === superid) {
       return (
         <div>
-          <div className="list-border status-bar">
-            {'pick your time' || 'you have picked your time'}
-            <button className="btn btn-warning">edit users</button>
-          </div>
-          <button className="btn btn-default" onClick={this.vote}>VOTE</button>
+          {this.state.project.title}
+          <p>you are superuser</p>
         </div>
       );
     } else {
       return (
-        <Super projectId={user.projectId} history={this.props.history} match={this.props.match} />
+        <div>
+          {this.state.project.title}
+          <p>you are not superuser</p>
+        </div>
       );
     }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="col-md-6 list-border">
-          <div className="row">
-            <Link className="btn btn-default col-md-2 back-button" to="/">
-              ㄑ Back
-            </Link>
-            <div className="col-md-4 noti-div">
-              <label className="noti-label">3 days left</label>
-            </div>
-          </div>
-          <EventData />
-        </div>
-        <div className="col-md-6 list-border">
-          {this.checkSuper(this.state)}
-        </div>
-      </div>
-    );
   }
 }
