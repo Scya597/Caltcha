af13b4c1dc60cd2db46b7808ee95dd4d17ae97d5
Maximus Wu <stib108@yahoo.com.tw>
2017-08-08 17:33:52 +0800

    :art:

--- src/components/manage_list.js
+++ src/components/manage_list.js
@@ -2,22 +2,50 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Panel, ProgressBar } from 'react-bootstrap';
 
+const ifvote = require('../utils/functions/ifvote');
+
 class manageList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = { sProj: [] };
+
+    this.getnormalvote = this.getnormalvote.bind(this);
+  }
+  componentDidMount() {
+    const b = [];
+    b.push(this.props.superProj.map(this.getnormalvote));
+    this.setState({ sProj: b });
+  }
+
+  getnormalvote = (obj) => {
+    const a = [];
+    a.push({
+      superuser: obj.superuser,
+      minDuration: obj.minDuration,
+      location: obj.location,
+      id: obj.id,
+      title: obj.title,
+      sum: obj.normaluser.length,
+      vote: ifvote(obj).normaluser.vote.length,
+      deadline: obj.deadline,
+    });
   }
 
   render() {
-    const superProjJSX = this.props.superProj.map((proj) => {
+    const superProjJSX = this.state.sProj.map((proj) => {
       return (
         <Link to={`/vote/${proj.id}/${this.props.user.id}/${proj.superuser}`} key={proj.id}>
           <Panel bsStyle="primary" className="btn-shadow">
               <h1>{proj.title}</h1>
               <div className="manage-list">
-                <h4>Duration: {proj.minDuration*0.5} hrs</h4>
+                <h4>Duration: {proj.minDuration * 0.5} hrs</h4>
                 <h4>Location: {proj.location}</h4>
-                <ProgressBar now={60} label={`${60}%`} bsStyle="danger"/>
+                <ProgressBar
+                  now={60}
+                  label={`${60}%`}
+                  bsStyle="danger"
+                />
               </div>
               <div className="danger">
                 <h5>Deadline: {proj.deadline}</h5>
